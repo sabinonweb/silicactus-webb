ca1451706b584bd6a05163fb356b1a61f7dcf048
sabinonweb <sabinonweb7@gmail.com>
2024-10-04 00:51:01 +0545

    [Update: Code optimized + animations improved(Flicker removed), and simplified it]

--- src/app/dashboard/AboutUs.jsx
+++ src/app/dashboard/AboutUs.jsx
@@ -1,13 +1,13 @@
 import MegaPhone from "../app/assets/Megaphone.png";
 import GraphicPen from "@/app/assets/Graphic.png";
 import WebPage from "@/app/assets/WebPage.png";
-import ContentContainer from "@/app/ui/about/ContentContainer";
+import ContentContainer from "@/app/ui/about/ContentContainer.tsx";
 
 export default function AboutUs() {
   return (
     <div className="flex flex-col md:flex-row flex-1 bg-[var(--primary-color)] items-center md:justify-evenly justify-center flex-wrap">
       <ContentContainer
-        Icon={GraphicPen}
+        Icon={MegaPhone}
         DisplayText="Engaging designs that inspire action"
         MainText=" At Silicactus, we believe that effective graphic design is at
                 the heart of every successful brand. Our talented team of
@@ -21,7 +21,7 @@ export default function AboutUs() {
       />
 
       <ContentContainer
-        Icon={WebPage}
+        Icon={GraphicPen}
         DisplayText="Crafting dynamic websites that perform"
         MainText=" At Silicactus, we believe that effective graphic design is at
                 the heart of every successful brand. Our talented team of
@@ -35,8 +35,8 @@ export default function AboutUs() {
       />
 
       <ContentContainer
-        Icon={MegaPhone}
-        DisplayText="Boost your brand's online presence "
+        Icon={WebPage}
+        DisplayText="Boost your brand's online presence"
         MainText=" At Silicactus, we believe that effective graphic design is at
                 the heart of every successful brand. Our talented team of
                 designers is dedicated to creating visually stunning and
--- src/app/ui/about/ContentContainer.tsx
+++ src/app/ui/about/ContentContainer.tsx
@@ -1,10 +1,83 @@
-import "@/app/styles/contentContainer.css";
+"use client";
 
-export default function ContentContainer({ img, text }) {
+import MobileContent from "@/app/assets/MobileContent.svg";
+import Image, { StaticImageData } from "next/image";
+import { motion } from "framer-motion";
+import Orange from "@/app/assets/Yellow.svg";
+import { Icons } from "@/app/lib/data";
+import React, { useState } from "react";
+
+interface Content {
+  Icon: StaticImageData;
+  DisplayText: string;
+  MainText: string;
+}
+
+export default function ContentContainer({
+  Icon,
+  DisplayText,
+  MainText,
+}: Content) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="container h-screen bg-gradient-to-r">
-      <div className="container-text ">boz</div>
-      <div className="text-container">jnakjsdkjz</div>
-    </div>
+    <motion.div
+      className="flex relative"
+      onHoverStart={() => setIsHovered(true)}
+      onHoverEnd={() => setIsHovered(false)}
+      transition={{ ease: "spring", duration: 0.5, opacity: 0.5 }}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
+    >
+      {!isHovered && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeIn", duration: 0.5 }}
+          exit={{ opacity: 0 }}
+        >
+          <Image
+            src={MobileContent}
+            alt="Mobile Content Box"
+            className="min-w-[300px] p-4"
+          />
+          <div className="absolute inset-0 flex justify-evenly items-center flex-col">
+            <Image
+              src={Icon}
+              alt={`Icon for the respective field`}
+              className="w-96 h-96 p-4"
+            />
+            <h1 className="text-[var(--primary-text-color)] text-4xl max-w-full text-center p-7">
+              {DisplayText}
+            </h1>
+          </div>
+        </motion.div>
+      )}
+
+      {isHovered && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ ease: "easeIn", duration: 0.5 }}
+          exit={{ opacity: 0 }}
+        >
+          <Image
+            src={Orange}
+            alt="Mobile Content Box"
+            className="min-w-[300px] p-4"
+          />
+          <div className="absolute inset-0 flex justify-center flex-col">
+            <h1 className="text-[var(--tertiary-text-color)] text-lg max-w-full text-center p-8">
+              <br></br>
+              {MainText}
+            </h1>
+            <button className="flex items-center justify-center">
+              <Icons.FaCircleChevronRight size="24px" />
+              <h1 className="p-3">Learn More</h1>
+            </button>
+          </div>
+        </motion.div>
+      )}
+    </motion.div>
   );
 }
