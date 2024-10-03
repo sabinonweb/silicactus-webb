515bdc78428af68d1ee56c4bf3eeb3b59481e9cc
sabinonweb <sabinonweb7@gmail.com>
2024-10-03 22:35:30 +0545

    [Update: Component Boxes]

---
+++ src/app/ui/about/ContentContainer.tsx
@@ -0,0 +1,10 @@
+import "@/app/styles/contentContainer.css";
+
+export default function ContentContainer({ img, text }) {
+  return (
+    <div className="container h-screen bg-gradient-to-r">
+      <div className="container-text ">boz</div>
+      <div className="text-container">jnakjsdkjz</div>
+    </div>
+  );
+}
